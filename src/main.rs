3ee061506da246d160ed894c8c2dc01df361e206
Jacob Keisling <jacob@keisling.me>
2024-03-12 00:00:51 -0500

    Add ffwd "transformer" (#5)
    * Fix clippy errors
    
    * Support multi-model
    
    * Genericize trainer

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use candle_core::{Device, Error, Result, Tensor};
 use candle_nn::{VarBuilder, VarMap};
 use clap::Parser;
 use nanogpt::config::pretrained_config::PretrainedConfig;
-use nanogpt::models::bigram::{self, Bigram};
+use nanogpt::models::{Model, ModelWrapper, WhichModel};
 use nanogpt::tokenizer::Tokenizer;
 use std::env;
 use std::path::PathBuf;
@@ -12,8 +12,8 @@ use std::process;
 #[command(version, long_about=None)]
 struct Args {
     /// Name of folder in model directory.
-    #[clap(short, long = "model-id", default_value = "bigram")]
-    model_id: String,
+    #[arg(short, long, default_value = "transformer")]
+    model_type: WhichModel,
 
     #[arg(short, long)]
     prompt: Option<String>,
@@ -22,17 +22,17 @@ struct Args {
     n_tokens: Option<usize>,
 }
 
-fn generate(
+fn generate<M: Model>(
     tokenizer: &Tokenizer,
-    model: &mut Bigram,
+    model: &mut M,
     prompt: &str,
     device: &Device,
     max_tokens: usize,
 ) -> Result<String> {
     let input_encoding = tokenizer
-        .encode(&prompt)
+        .encode(prompt)
         .map_err(|_| Error::Msg("Tokenizer error".into()))?;
-    let idx_1d = Tensor::new(input_encoding.ids, &device)?;
+    let idx_1d = Tensor::new(input_encoding.ids, device)?;
     let idx = idx_1d.reshape((1, idx_1d.dims1()?))?;
     println!("Tokenized");
     let data = model.generate(&idx, max_tokens)?;
@@ -50,7 +50,8 @@ fn main() {
 
     let cwd = env::current_dir().unwrap();
     // Hardcode to bigram for now
-    let config_path: PathBuf = cwd.join("models/bigram/config.json");
+    let model_name: String = args.model_type.into();
+    let config_path: PathBuf = cwd.join(format!("models/{}/config.json", model_name));
     if !config_path.exists() {
         eprintln!(
             "Error: config file not found at expected path: {:?}",
@@ -89,25 +90,19 @@ fn main() {
     // TODO: Factor this out once we make this multi-model
     let mut varmap = VarMap::new();
     let vs = VarBuilder::from_varmap(&varmap, candle_core::DType::F32, &device);
-    let mut model = bigram::Bigram::new(
-        vs,
-        &bigram::Config {
-            vocab_size: tokenizer.get_vocab_size(),
-        },
-    )
-    .unwrap();
+    let mut model: ModelWrapper = ModelWrapper::from_config(vs, &config).unwrap();
 
     // Get weights if exist, else bail
-    let weight_path = cwd.join(format!("models/{}/model.safetensors", args.model_id));
+    let weight_path = cwd.join(format!("models/{}/model.safetensors", model_name));
     if weight_path.exists() {
-        println!("Loading {} model", args.model_id);
+        println!("Loading {} model", model_name);
         varmap.load(weight_path).unwrap();
     } else {
         println!("Fail!");
     }
 
-    let prompt = args.prompt.unwrap_or_else(|| " ".to_string());
-    let max_tokens = args.n_tokens.unwrap_or_else(|| 20);
+    let prompt = args.prompt.unwrap_or(" ".to_string());
+    let max_tokens = args.n_tokens.unwrap_or(20);
     println!(
         "{:?}",
         generate(&tokenizer, &mut model, &prompt, &device, max_tokens)
--- src/models.rs
+++ src/models.rs
@@ -1,1 +1,61 @@
+use self::{bigram::Bigram, transformer::Transformer};
+
+use super::config::pretrained_config::PretrainedConfig;
+use candle_core::{Result, Tensor};
+use candle_nn::{Module, VarBuilder};
+use clap::ValueEnum;
+
 pub mod bigram;
+pub mod transformer;
+
+pub trait Model: Sized + Module {
+    fn generate(&mut self, idx: &Tensor, max_new_tokens: usize) -> Result<Tensor>;
+    fn from_config(vs: VarBuilder, cfg: &PretrainedConfig) -> Result<Self>;
+}
+
+pub enum ModelWrapper {
+    Bigram(bigram::Bigram),
+    Transformer(transformer::Transformer),
+}
+
+impl Module for ModelWrapper {
+    fn forward(&self, xs: &Tensor) -> Result<Tensor> {
+        match self {
+            Self::Bigram(b) => b.forward(xs),
+            Self::Transformer(t) => t.forward(xs),
+        }
+    }
+}
+
+impl Model for ModelWrapper {
+    fn generate(&mut self, idx: &Tensor, max_new_tokens: usize) -> Result<Tensor> {
+        match self {
+            Self::Bigram(b) => b.generate(idx, max_new_tokens),
+            Self::Transformer(t) => t.generate(idx, max_new_tokens),
+        }
+    }
+    fn from_config(vs: VarBuilder, cfg: &PretrainedConfig) -> Result<Self> {
+        match cfg.architecture.as_str() {
+            "bigram" => Ok(ModelWrapper::Bigram(Bigram::from_config(vs, cfg)?)),
+            "transformer" => Ok(ModelWrapper::Transformer(Transformer::from_config(
+                vs, cfg,
+            )?)),
+            _ => Err(candle_core::Error::Msg("Invalid architecture type".into())),
+        }
+    }
+}
+
+#[derive(ValueEnum, Clone, Debug, PartialEq)]
+pub enum WhichModel {
+    Bigram,
+    Transformer,
+}
+
+impl Into<String> for WhichModel {
+    fn into(self) -> String {
+        match self {
+            WhichModel::Bigram => "bigram".into(),
+            WhichModel::Transformer => "transformer".into(),
+        }
+    }
+}
